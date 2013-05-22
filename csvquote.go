933a9592c31dbf3abf80eddefd0d6e46e186b878
dbro <danbrown@gmail.com>
2013-05-22 10:53:07 +0200

    get rid of global variables, create functions with parameters to be re-used

--- csvquote.go
+++ csvquote.go
@@ -7,21 +7,26 @@ import (
 	"os"
 )
 
-var restoremode = flag.Bool("u", false, "restore the original separator characters")
-var delimiter = flag.String("d", ",", "field separator character")
-var delimitertab = flag.Bool("t", false, "use tab as field separator (overrides -d parameter)")
-var quotechar = flag.String("q", "\"", "field quoting character")
-var recordsep = flag.String("r", "\n", "record separator character")
-
-var delimiterNonprintingByte byte = 31
-var recordsepNonprintingByte byte = 30
-var delimiterByte, quotecharByte, recordsepByte byte
+const delimiterNonprintingByte,	recordsepNonprintingByte byte = 31, 30
 
 func main() {
+	restoremode := flag.Bool("u", false, "restore the original separator characters")
+	delimiter := flag.String("d", ",", "field separator character")
+	delimitertab := flag.Bool("t", false, "use tab as field separator (overrides -d parameter)")
+	quotechar := flag.String("q", "\"", "field quoting character")
+	recordsep := flag.String("r", "\n", "record separator character")
 	flag.Parse() // Scans the arg list and sets up flags
 	if *delimitertab {
 		*delimiter = "\t"
 	}
+	delimiterByte := byte((*delimiter)[0])
+	quotecharByte := byte((*quotechar)[0])
+	recordsepByte := byte((*recordsep)[0])
+	mapFunction := substituteNonprintingChars(delimiterByte, quotecharByte, recordsepByte)
+	if *restoremode {
+		mapFunction = restoreOriginalChars(delimiterByte, recordsepByte)
+	}
+
 	var input *os.File
 	if flag.NArg() > 0 {
 		var err error
@@ -33,17 +38,8 @@ func main() {
 		input = os.Stdin
 	}
 
-	mapFunction := replaceOriginalChars
-	if *restoremode {
-		mapFunction = restoreOriginalChars
-	}
 	stateQuoteInEffect := false         // only used in replace mode
 	stateMaybeEscapedQuoteChar := false // only used in replace mode
-
-	delimiterByte = byte((*delimiter)[0])
-	quotecharByte = byte((*quotechar)[0])
-	recordsepByte = byte((*recordsep)[0])
-
 	data := make([]byte, 1024)
 	for {
 		if count, err := input.Read(data); err != nil {
@@ -65,42 +61,46 @@ func main() {
 	}
 }
 
-func replaceOriginalChars(c byte, stateQuoteInEffect bool, stateMaybeEscapedQuoteChar bool) (byte, bool, bool) {
-	d := c // default
-	if stateMaybeEscapedQuoteChar {
-		if c != quotecharByte {
-			// this is the end of a quoted field
-			stateQuoteInEffect = false
+func substituteNonprintingChars(delimiterByte byte, quotecharByte byte, recordsepByte byte) (func(byte, bool, bool) (byte, bool, bool)) {
+	return func(c byte, stateQuoteInEffect bool, stateMaybeEscapedQuoteChar bool) (byte, bool, bool) {
+		d := c // default
+		if stateMaybeEscapedQuoteChar {
+			if c != quotecharByte {
+				// this is the end of a quoted field
+				stateQuoteInEffect = false
+			}
+			stateMaybeEscapedQuoteChar = false
+		} else if stateQuoteInEffect {
+			switch c {
+			case quotecharByte:
+				// this is either an escaped quote char or the end of a quoted
+				// field. need to read one more character to decide which
+				stateMaybeEscapedQuoteChar = true
+			case delimiterByte:
+				d = delimiterNonprintingByte
+			case recordsepByte:
+				d = recordsepNonprintingByte
+			}
+		} else {
+			// quote not in effect
+			if c == quotecharByte {
+				stateQuoteInEffect = true
+			}
 		}
-		stateMaybeEscapedQuoteChar = false
-	} else if stateQuoteInEffect {
-		switch c {
-		case quotecharByte:
-			// this is either an escaped quote char or the end of a quoted
-			// field. need to read one more character to decide which
-			stateMaybeEscapedQuoteChar = true
-		case delimiterByte:
-			d = delimiterNonprintingByte
-		case recordsepByte:
-			d = recordsepNonprintingByte
-		}
-	} else {
-		// quote not in effect
-		if c == quotecharByte {
-			stateQuoteInEffect = true
-		}
+		return d, stateQuoteInEffect, stateMaybeEscapedQuoteChar
 	}
-	return d, stateQuoteInEffect, stateMaybeEscapedQuoteChar
 }
 
-func restoreOriginalChars(c byte, stateQuoteInEffect bool, stateMaybeEscapedQuoteChar bool) (byte, bool, bool) {
-	// need to have same input/output parameters as replaceOriginalChars()
-	// so the state variables are included but not used
-	switch c {
-	case delimiterNonprintingByte:
-		return delimiterByte, false, false
-	case recordsepNonprintingByte:
-		return recordsepByte, false, false
+func restoreOriginalChars(delimiterByte byte, recordsepByte byte) (func(byte, bool, bool) (byte, bool, bool)) {
+	return func(c byte, stateQuoteInEffect bool, stateMaybeEscapedQuoteChar bool) (byte, bool, bool) {
+		// need to have same input/output parameters as replaceOriginalChars()
+		// so the state variables are included but not used
+		switch c {
+		case delimiterNonprintingByte:
+			return delimiterByte, false, false
+		case recordsepNonprintingByte:
+			return recordsepByte, false, false
+		}
+		return c, false, false
 	}
-	return c, false, false
 }
