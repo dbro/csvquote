0126369f62fd96bd036c8050e9140f46ef2470a8
dbro <danbrown@gmail.com>
2013-12-02 22:13:21 +0100

    parse command line options

--- csvquote.c
+++ csvquote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-//#include <string.h>
+#include <unistd.h>
 #include "dbg.h"
 
 #define READ_BUFFER_SIZE 4096
@@ -9,16 +8,15 @@
 #define NON_PRINTING_RECORD_SEPARATOR 0x1E
 
 /*
-TODO: handle multi-byte characters and unicode and utf-8 etc
-TODO: parse options
+TODO: verify that it handles multi-byte characters and unicode and utf-8 etc
 */
 
-typedef int (*translator)(const char, const char, const char, char *);
+typedef void (*translator)(const char, const char, const char, char *);
+typedef enum { HEADER_MODE, RESTORE_MODE, SANITIZE_MODE } operation_mode;
 
-int restore(const char delimiter, const char quotechar, const char recordsep, char *c) {
+void restore(const char delimiter, const char quotechar, const char recordsep, char *c) {
     // the quotechar is not needed when restoring, but we include it
     // to keep the function parameters consistent for both translators
-    //debug("attempting restoration of %c", *c);
     switch (*c) {
         case NON_PRINTING_FIELD_SEPARATOR:
             *c = delimiter;
@@ -28,10 +26,10 @@ int restore(const char delimiter, const char quotechar, const char recordsep, ch
             break;
         // no default case needed
     }
-    return 0;
+    return;
 }
 
-int sanitize(const char delimiter, const char quotechar, const char recordsep, char *c) {
+void sanitize(const char delimiter, const char quotechar, const char recordsep, char *c) {
     // maintain the state of quoting inside this function
     // this is OK because we need to read the file
     // sequentially (not in parallel) because the state
@@ -61,52 +59,107 @@ int sanitize(const char delimiter, const char quotechar, const char recordsep, c
             isQuoteInEffect = true;
         }
     }
-    return 0;
+    return;
 }
 
-int copy_file(FILE *in, FILE *out, const translator trans, const char del, const char quo, const char rec)
-{
+int copy_file(FILE *in, const operation_mode op_mode,
+const char del, const char quo, const char rec) {
     char buffer[READ_BUFFER_SIZE];
     size_t nbytes;
     char *c, *stopat;
 
+    translator trans = sanitize; // default
+    if (op_mode == RESTORE_MODE) {
+        trans = restore;
+    } else if (op_mode == HEADER_MODE) {
+        debug("header mode goes here");
+        return 0; // exit
+    }
+
+    debug("copying file with d=%d\tq=%d\tr=%d", del, quo, rec);
     while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), in)) != 0)
     {
         stopat = buffer + (nbytes);
         for (c=buffer; c<stopat; c++) {
-            //debug("attempting translation of %c", *c);
-            check((*trans)(del, quo, rec, c) == 0,
-                "translator returned unexpected result.");
+            (*trans)(del, quo, rec, c); // no error checking inside this loop
         }
-        check(fwrite(buffer, sizeof(char), nbytes, out) == nbytes,
+        check(fwrite(buffer, sizeof(char), nbytes, stdout) == nbytes,
             "Failed to write %zu bytes\n", nbytes);
     }
     return 0;
 
 error:
+    //if (in) { fclose(input); } // is this appropriate?
     return 1;
 }
 
-int main(int argc, char *argv[])
-{
-    //debug("Started main");
+int main(int argc, char *argv[]) {
     // default parameters
+    FILE *input = NULL;
     char del = ',';
     char quo = '"';
     char rec = '\n';
-    bool restoremode = false;
-    bool headermode = false; // supercedes other parameters
+    operation_mode op_mode = SANITIZE_MODE;
 
-    translator trans = sanitize; // default
-    if (restoremode) {
-        trans = restore;
+    int opt;
+    while ((opt = getopt(argc, argv, "hud:tq:r:")) != -1) {
+        switch (opt) {
+            case 'h':
+                op_mode = HEADER_MODE;
+                break;
+            case 'u':
+                op_mode = RESTORE_MODE;
+                break;
+            case 'd':
+                del = optarg[0];
+                break;
+            case 't':
+                del = '\t';
+                break;
+            case 'q':
+                quo = optarg[0];
+                break;
+            case 'r':
+                rec = optarg[0];
+                break;
+            case ':':
+                // -d or -q or -r without operand
+                fprintf(stderr,
+                    "Option -%c requires an operand\n", optopt);
+                goto usage;
+            case '?':
+                goto usage;
+            default:
+                fprintf(stderr,
+                    "Unrecognized option: '-%c'\n", optopt);
+                goto usage;
+        }
     }
 
-    //debug("starting to copy data ...");
-    check(copy_file(stdin, stdout, trans, del, quo, rec) == 0, "copy_file failed.");
-    //debug("Completed main");
+    // Process stdin or file names
+    if (optind >= argc) {
+        check(copy_file(stdin, op_mode, del, quo, rec) == 0,
+            "failed to copy from stdin");
+    } else {
+        // supports multiple file names
+        int i;
+        for (i=optind; i<argc; i++) {
+            input = fopen(argv[i], "r");
+            check(input != 0, "failed to open file %s", argv[optind]);
+            check(copy_file(input, op_mode, del, quo, rec) == 0,
+                "failed to copy from file %s", argv[i]);
+            if (input) { fclose(input); }
+        }
+    }
+
+    if (input) { fclose(input); }
     return 0;
 
+usage:
+    fprintf(stderr, "Usage: %s [-hud:tq:r:] [file]\n", argv[0]);
+    return 1;
+
 error:
+    if (input) { fclose(input); }
     return 1;
 }
